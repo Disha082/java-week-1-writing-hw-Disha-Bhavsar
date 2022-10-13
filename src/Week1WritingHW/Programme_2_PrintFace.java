8a79643ffa4bfd100a11b0f1742f1dc1f4656ce4
Disha082 <disha130308@gmail.com>
2022-10-13 23:57:12 +0100

    java-week-1-writing-hw-Disha-Bhavsar

---
+++ src/Week1WritingHW/Programme_2_PrintFace.java
@@ -0,0 +1,14 @@
+package Week1WritingHW;
+//2. Write a Java program to print a face.
+
+public class Programme_2_PrintFace {
+    public static void main (String [] args){
+        System.out.println(" +\"\"\"\"\"+");
+        System.out.println("[| o o |] ");
+        System.out.println(" |  ^  |  ");
+        System.out.println(" | . . |  ");
+        System.out.println(" |  _  |  ");
+        System.out.println(" +______+");
+
+    }
+}
---
+++ src/Week1WritingHW/Programme_4_RightTriangle.java
@@ -0,0 +1,14 @@
+package Week1WritingHW;
+//4. Write a Java Program to print below triangle
+
+public class Programme_4_RightTriangle {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+
+    }
+}
